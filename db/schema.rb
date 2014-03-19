5e5ff226bcc7eed70f05f5b46c4ffa4051421414
jesical <chensongL516@gmail.com>
2014-03-19 15:23:31 +0800

    fix bug that rails can not start

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140312135359) do
+ActiveRecord::Schema.define(version: 20140319044802) do
 
   create_table "microposts", force: true do |t|
     t.string   "content"
